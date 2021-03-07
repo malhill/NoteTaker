34304e5f15a93d3373024b8585b60a14d75568ea
malhill <74620676+malhill@users.noreply.github.com>
2021-03-06 21:35:45 -0500

    finally got the notetaker to persist information

--- develop/backend.js
+++ develop/backend.js
@@ -30,7 +30,7 @@ app.get('/api/notes', (request, response) => {
     fs.readFile('db/db.json', 'utf8', (err, data) => {
         if (err) throw err;
         // console.log(data);
-        response.json(data);
+        response.json(JSON.parse(data))
         // console.log(data);
     }); 
 });
