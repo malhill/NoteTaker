7e4299e601aa241be22c46706f59d9130e246e37
malhill <74620676+malhill@users.noreply.github.com>
2021-03-31 12:27:58 -0400

    cleaned up comments in backend.js

--- backend.js
+++ backend.js
@@ -24,23 +24,10 @@ app.get("/notes", (request, response) => {
 
 app.get('/api/notes', (request, response) => {
     response.json(db);
-    // if (err) throw err;
-
-    // fs.readFile('db/db.json', 'utf8', (err, data) => {
-    //     response.json(data);
-    //     // console.log('get' + data);
-    // });
 });
 
 app.post("/api/notes", (request, response) => {
-    // fs.readFile('db/db.json', 'utf8', (err, data) => {
-        // if (err) throw err;
-        // console.log(data);
-
         const newNote = request.body;
-
-        // let userNotes = JSON.parse(data);
-        // let userNotes = response.json(JSON.parse(data));
 
         if(db.length == 0) {
             newNote.id=0;
@@ -48,15 +35,11 @@ app.post("/api/notes", (request, response) => {
             newNote.id = parseInt(db[db.length - 1].id) + 1;
         }
 
-        // newNote.id = parseInt(userNotes[userNotes.length - 1].id) + 1; // <---- Anthony was here!
-
         // console.log((userNotes[userNotes.length - 1].id) + 1);
-        console.log(newNote);
-
+        // console.log(newNote);
 
         db.push(newNote);
         // console.log(userNotes);
-
 
         fs.writeFile('db/db.json', JSON.stringify(db), (err, data) => {
             if (err) throw err;
@@ -69,22 +52,11 @@ app.post("/api/notes", (request, response) => {
     });
 
 app.delete("/api/notes/:id", (request, response) => {
-    // fs.readFile('db/db.json', 'utf8', (err, data) => {
-        // if (err) throw err;
-        // console.log(data);
-
-        // response.json(db);
-
-
-        // const deleteNotes = JSON.parse(data);
-
         const deleteId = request.params.id;
 
         // console.log(deleteId);
+        // console.log(db);
 
-        console.log(db);
-
-        //.toString() Just in case we need this...
         for (let i = 0; i < db.length; i++) {
             console.log(db[i].id)
             if (deleteId == db[i].id) {
